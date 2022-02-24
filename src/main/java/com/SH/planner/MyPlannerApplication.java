13e205b1fb0a147db505d2435b0b92f502af80a3
Sohyun <sh-119@naver.com>
2022-02-24 19:43:42 +0900

    MyPlanner [이소현] 프로젝트 생성

---
+++ src/main/java/com/SH/planner/MyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.SH.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SH/planner/MyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SH.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
